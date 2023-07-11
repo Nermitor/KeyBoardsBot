4215b6eee99a23c82e916841723cb47568ba839a
Nermitor <dmatyukhin2006@mail.ru>
2023-07-11 13:39:24 +0700

    Initial commit

---
+++ keyboards/reply.py
@@ -0,0 +1,10 @@
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
+
+answer_dogs_button = KeyboardButton(text='Собак')  # Кнопка
+answer_cats_button = KeyboardButton(text='Котов')  # Кнопка
+
+cats_dogs_keyboard = ReplyKeyboardMarkup(keyboard=[
+    [answer_dogs_button, answer_cats_button],
+], resize_keyboard=True, one_time_keyboard=True)  # Клавиатура
+
+
